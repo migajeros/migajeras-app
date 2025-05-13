ce63fe344b4711d9bcb243686d9d33d915f3ef69
migajeros <migajerosa@gmail.com>
2025-05-13 05:29:45 -0500

    Import globals.css en _app.js para aplicar estilos

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,19 @@
 // pages/_app.js
+import '../styles/globals.css'
 import dynamic from 'next/dynamic'
+import Layout from '../components/Layout'
 
-// Carga el provider **solo en cliente** (no SSR)
 const ThirdwebProviderNoSSR = dynamic(
-  () => import('@thirdweb-dev/react').then(mod => mod.ThirdwebProvider),
+  () => import('@thirdweb-dev/react').then(m => m.ThirdwebProvider),
   { ssr: false }
 )
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThirdwebProviderNoSSR desiredChainId={1 /* ChainId.Mainnet */}>
-      <Component {...pageProps} />
+    <ThirdwebProviderNoSSR desiredChainId={1}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ThirdwebProviderNoSSR>
   )
 }
